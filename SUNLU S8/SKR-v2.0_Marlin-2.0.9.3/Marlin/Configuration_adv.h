03a22aa849cb1bd05ca4db9f0b911b09fb493e45
Brendan Bone <brendan.bone@exterro.com>
2022-04-30 13:19:46 -0600

    s8: z align and tramming guide

--- SUNLU S8/SKR-v2.0_Marlin-2.0.9.3/Marlin/Configuration_adv.h
+++ SUNLU S8/SKR-v2.0_Marlin-2.0.9.3/Marlin/Configuration_adv.h
@@ -928,7 +928,7 @@
  * Z Steppers Auto-Alignment
  * Add the G34 command to align multiple Z steppers using a bed probe.
  */
-//#define Z_STEPPER_AUTO_ALIGN
+#define Z_STEPPER_AUTO_ALIGN
 #if ENABLED(Z_STEPPER_AUTO_ALIGN)
   // Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
   // If not defined, probe limits will be used.
@@ -985,11 +985,11 @@
 //
 // Add the G35 command to read bed corners to help adjust screws. Requires a bed probe.
 //
-//#define ASSISTED_TRAMMING
+#define ASSISTED_TRAMMING
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probe points.
-  #define TRAMMING_POINT_XY { {  20, 20 }, { 180,  20 }, { 180, 180 }, { 20, 180 } }
+  #define TRAMMING_POINT_XY { {  40, 40 }, { 260,  40 }, { 260, 260 }, { 40, 260 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
